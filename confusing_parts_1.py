c98143f760142edb49fccb1a7f2a7410a22baf90
Karvendhan M <karvsmech@gmail.com>
2019-04-11 11:07:01 +0530

    Feature_addition: toy linear regression addition

--- confusing_parts_1.py
+++ confusing_parts_1.py
@@ -187,9 +187,107 @@ sess.run(zero_node)
 ########
 # Initializers
 import tensorflow as tf
-
+const_init_node = tf.constant_initializer(0.)
+# const_init_node = tf.constant(0.)
+count_variable = tf.get_variable("count", [], initializer=const_init_node)
+sess = tf.Session()
+sess.run(const_init_node)
+print(count_variable)
+sess.run([count_variable])
+sess.run(count_variable)
 
 ########
+
+########
+
+import tensorflow as tf
+const_init_node = tf.constant_initializer(0.)
+count_variable = tf.get_variable('count', [], initializer=const_init_node)
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+sess.run([count_variable])
+sess.run(count_variable)
+
+########
+
+########
+
+### two ways to assign a value to a variable 1) assign and 2) using initializers
+## using assign
+
+import tensorflow as tf
+count_variable = tf.get_variable('count', [])
+zero_node = tf.constant(0.)
+assign_node = tf.assign(count_variable, zero_node)
+sess = tf.Session()
+sess.run(assign_node)
+sess.run(count_variable)
+sess.run(zero_node)
+
+## using initializer
+
+import tensorflow as tf
+const_init = tf.constant_initializer(0.)
+count_variable = tf.get_variable('count', [], initializer=const_init)
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+sess.run(count_variable)
+
+########
+
+########
+# Toy linear regression problem
+
+import tensorflow as tf
+
+### build the graph
+## first set up the parameters
+m = tf.get_variable('m',[], initializer=tf.constant_initializer(0.))
+b = tf.get_variable('b',[], initializer=tf.constant_initializer(0.))
+init = tf.global_variables_initializer()
+
+## then let us set up the computations
+input_placeholder = tf.placeholder(tf.float32)
+output_placeholder = tf.placeholder(tf.float32)
+
+x = input_placeholder
+y = output_placeholder
+
+y_guess = m * x + b
+
+loss = tf.square(y - y_guess)
+
+## finally, set up the optimizer and minimization node
+optimizer = tf.train.GradientDescentOptimizer(1e-3)
+train_op = optimizer.minimize(loss)
+
+### start the session
+sess = tf.Session()
+sess.run(init)
+
+### perform the training loop
+import random
+
+# set up the problem
+true_m = random.random()
+true_b = random.random()
+
+for update_i in range(100000):
+    input_data = random.random()
+    output_data = true_m * input_data + true_b
+
+    _loss, _ = sess.run([loss, train_op], feed_dict={input_placeholder:input_data, output_placeholder:output_data})
+    print(update_i, _loss)
+
+print("True parameters:     m=%.4f, b=%.4f" % (true_m, true_b))
+print("Learned parameters:  m=%.4f, b=%.4f" % tuple(sess.run([m, b])))
+
+########
+
+
+
 
 
 
